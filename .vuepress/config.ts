e8913c07c0e8ffe9bb49009a426433550ce8715c
yupi <592789970@qq.com>
2022-06-22 20:48:50 +0800

    add 星球项目 add 51.la 统计

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -13,6 +13,9 @@ export default defineUserConfig({
     title: '编程导航知识星球',
     description: '帮助你学习编程的小圈子',
     head: [
+        ['script', {}, `
+            !function(p){"use strict";!function(t){var s=window,e=document,i=p,c="".concat("https:"===e.location.protocol?"https://":"http://","sdk.51.la/js-sdk-pro.min.js"),n=e.createElement("script"),r=e.getElementsByTagName("script")[0];n.type="text/javascript",n.setAttribute("charset","UTF-8"),n.async=!0,n.src=c,n.id="LA_COLLECT",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:"JjDPBvNXNcI9wXx4",ck:"JjDPBvNXNcI9wXx4",autoTrack:true,hashMode:true});
+        `],
         ['link', { rel: 'icon', href: '/favicon.ico' }],
         ['meta', { name: 'keywords', content: '编程学习圈子,程序员学习,程序员,代码,编程项目,编程导航,程序员鱼皮'}],
         ['meta', {
